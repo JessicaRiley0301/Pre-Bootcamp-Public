73c9f432f19eff365f46d9d4ff968f331b351420
JessicaRiley0301 <jessica.arlene.riley@gmail.com>
2021-04-26 14:46:39 -0700

    loopchallenge1commit

---
+++ java/loopchallenges.js
@@ -0,0 +1,17 @@
+for (var i=0; i<20; i++) {
+    if (i % 2 != 0){
+    console.log (i)
+    }
+}
+
+for (i=4, i>-3.5, i-1.5) {
+    console.log (i)
+}
+
+for (i=0, i<100, i++);
+    y=sum (i)
+    console.log (y)
+}
+
+for (n=0, n<10000000, ie8)
+    console.log (n)
