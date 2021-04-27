5e133e41151489d718cd343d2082588d159fe450
JessicaRiley0301 <jessica.arlene.riley@gmail.com>
2021-04-27 13:46:08 -0700

    pizzeriacommit

---
+++ java/dojopizzeria.js
@@ -0,0 +1,13 @@
+function pizzaoven (crustType, sauceType, cheeses, toppings) {
+    var pizza={}
+    pizza.crustType;
+    pizza.sauceType;
+    pizza.cheeses;
+    pizza.toppings;
+    return pizza;
+}
+
+var p1=pizzaoven ("deep dish", "traditional", ["mozzarella"],["pepperoni", "sausage"]);
+var p2=pizzaoven ("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
+var p3=pizzaoven ("cheesestuffcrust", "vodka", ["mozzarella"], ["pineapple", "ham"]);
+var p4=pizzaoven ("flatbread", "whitesauce", ["ricotta"], ["slicedtomato", "basil"]);
