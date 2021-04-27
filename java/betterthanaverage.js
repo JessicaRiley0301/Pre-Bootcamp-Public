1eed9ea96ba0921bc76fd4cfa02d785f09bfa12a
JessicaRiley0301 <jessica.arlene.riley@gmail.com>
2021-04-26 22:22:41 -0700

    betterthanaveragecommit

---
+++ java/betterthanaverage.js
@@ -0,0 +1,13 @@
+function betterThanAverage(arr) {
+    var sum = 0;
+        for (i=0, i<arr.length; i++) {
+        sum (arr[i]);
+        }
+    var count = 0
+        if i > sum (arr[i]) {
+            count(arr.length[i]);
+        }
+    return count;
+}
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result);
---
+++ java/fibonacci.js
@@ -0,0 +1,11 @@
+function fibonacciArray(n) {
+    // the [0, 1] are the starting values of the array to calculate the rest from
+    var fibArr = [0, 1];
+    for (let i = 2; i < n + 1; i++){
+        fibarr.push(fibarr[i - 2] + fibarr[i -1])
+    }
+    return fibArr;
+}
+   
+var result = fibonacciArray(10);
+console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
